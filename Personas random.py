d9b40432045d87dfe7320a99efcf980b631b9ce1
jose <josevega1999,16@gmail.com>
2021-12-13 19:40:47 -0400

    Selección de estudiantes José vega y Norlie Rojas V2

--- Personas random.py
+++ Personas random.py
@@ -1,8 +1,17 @@
+## Integrantes:
+# José Manuel Vega R. 27.000.723
+# Norlie Rojas. 26.707.950
 import random
 estudiantes = ["Norlie Rojas", "Jose Vega", "Wuilmer Pulgar", "Ashly", "Cesar", "Diego Tineo", "Enrique Salazar" ,"Estefano Ramos", "Hector Salazar", "Jose Agreda", "Jose Angel", "Jose Diaz", "Maria Veronica", "Mauricio"]
-
+fin = 'n'
 ## Imprime 2 estudiantes de la lista
-resp=random.choice(estudiantes)
-print (resp)
-estudiantes.remove(resp)
-print (random.choice(estudiantes))
+while (fin == 'n' or fin=='N'):
+    resp=random.choice(estudiantes)
+    print (resp)
+    estudiantes.remove(resp)
+    print (random.choice(estudiantes))
+    print("¿Salir? S/N")
+    fin = input()
+    while (fin!='n' and fin!='N' and fin!='s' and fin!='S'):
+        print("Ingrese una opción correcta. S/N")
+        fin = input()
